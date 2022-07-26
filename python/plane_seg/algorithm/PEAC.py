d67b5c3e448682df490ba912cbd443417e001283
Bogdan Rzheusski <bogdanrzheusski@gmail.com>
2022-07-26 18:02:41 +0000

    PEAC Preprocessing rewritten + minor changes

--- python/plane_seg/algorithm/PEAC.py
+++ python/plane_seg/algorithm/PEAC.py
@@ -16,17 +16,22 @@ __all__ = ["PEAC"]
 
 
 class PEAC(Algorithm.Algorithm):
-    def __init__(self, container_name: str, cfg_path: Path, pcd_path: Path):
+    def __init__(
+        self,
+        container_name: str,
+        cfg_path: Path,
+        pcd_path: Path,
+        size: tuple = (640, 480),
+    ):
         self.container_name = container_name
         self.cfg_path = cfg_path
         self.pcd_path = pcd_path
-        self.pcd_name = Path(self.pcd_path).name[:-4]
+        self.pcd_name = Path(self.pcd_path).stem
+        self.size = size
         self._cfg = None
         self._alg_input_dir = Path("input")
         self._alg_output_dir = Path("output")
-        self._alg_artifact_name = Path(
-            "output/" + self.pcd_name + "/" + self.pcd_name + ".npy"
-        )
+        self._alg_artifact_name = Path(self.pcd_name + "/" + self.pcd_name + ".npy")
         self._parameter_list = (
             "loop",
             "debug",
@@ -52,13 +57,28 @@ class PEAC(Algorithm.Algorithm):
 
     def _preprocess_input(self) -> Collection[str]:
         pcd_name = self.__preprocess_point_cloud().name
-        cfg_name = self._cfg.write(self._alg_input_dir / "peac.ini").name
+        cfg_name = self._cfg.write(self._alg_input_dir / self.cfg_path.name).name
 
         return [pcd_name, cfg_name]
 
     def __preprocess_point_cloud(self) -> Path:
-        pcd_path = str(self._alg_input_dir / self.pcd_path.stem) + ".pcd"
-        pcd = pypcd.PointCloud.from_path(str(self.pcd_path))
+        pcd_path = self._alg_input_dir / (self.pcd_path.stem + ".pcd")
+
+        pcd = o3d.io.read_point_cloud(str(self.pcd_path))
+
+        cloud = o3d.geometry.PointCloud()
+        cloud.points = o3d.utility.Vector3dVector(pcd.points)
+
+        o3d.io.write_point_cloud(str(pcd_path), cloud)
+
+        pcd = pypcd.PointCloud.from_path(str(pcd_path))
+
+        data = pcd.pc_data.view(np.float32)
+        meta = pcd.get_metadata()
+        meta["height"] = self.size[0]
+        meta["width"] = self.size[1]
+
+        pcd = pypcd.make_xyz_point_cloud(data, meta)
         pcd.save_pcd(pcd_path)
 
         return Path(self.pcd_path)
@@ -86,7 +106,7 @@ class PEAC(Algorithm.Algorithm):
         return self._alg_output_dir / self._alg_artifact_name
 
     def _output_to_labels(self, output_path: Path) -> np.ndarray:
-        labels = np.load(self._alg_artifact_name)
+        labels = np.load(output_path)
 
         return labels
 
